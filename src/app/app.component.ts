fae723f0f43548881a771f2c4d6801c5cbc0f826
Jorge Vergara <javebratt@gmail.com>
2018-03-03 09:56:58 -0500

    Sets up the authentication observer.
    The goal is to redirect people depending on their state.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,31 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { AngularFireAuth } from 'angularfire2/auth';
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: 'app.html',
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    afAuth: AngularFireAuth
+  ) {
+    afAuth.authState.subscribe(user => {
+      if (!user) {
+        this.rootPage = 'LoginPage';
+      } else {
+        if (!user.emailVerified) {
+          this.rootPage = 'WaitingPage';
+        } else {
+          this.rootPage = HomePage;
+        }
+      }
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +37,3 @@ export class MyApp {
     });
   }
 }
-
